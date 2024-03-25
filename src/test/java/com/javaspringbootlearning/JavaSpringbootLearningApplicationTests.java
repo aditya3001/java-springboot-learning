ec45db66f2e4faac42517237b238ca28a06f65a2
adityajain <adityajainmathura@gmail.com>
2024-03-25 12:58:55 +0530

    initial commit

---
+++ src/test/java/com/javaspringbootlearning/JavaSpringbootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspringbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
